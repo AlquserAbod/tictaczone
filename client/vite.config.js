772528169fc21c590e5976227a6c527bab6a9e70
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-09-10 21:46:39 +0300

    edit env location in client

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,6 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export default defineConfig({
   plugins: [react()],
@@ -14,7 +10,7 @@ export default defineConfig({
   },
   proxy: {
     '/': {
-      target: process.env.VITE_SERVER_URL || 'http://localhost:5000',
+      target: 'http://localhost:5000',
     }
   },
 })
