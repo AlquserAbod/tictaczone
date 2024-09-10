c44e056cb853b86a7fb84ea8214f11f1855b7a90
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-09-10 21:48:35 +0300

    edit server.js remove statics

--- server/server.js
+++ server/server.js
@@ -13,11 +13,7 @@ dotenv.config({path: path.join(__dirname,"/.env")})
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname,"/client/dist")));
 
-app.get("*",(req,res) => {
-  res.sendFile(path.join(__dirname,"client","dist","index.html"))
-})
 
 
 const PORT = process.env.PORT || 5000;
