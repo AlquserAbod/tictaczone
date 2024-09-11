38606dfcf7b45b8df3c9a390c8353bc522181718
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-09-11 20:40:53 +0300

    ready to deploy to render

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ dotenv.config()
 import { app, server } from './socket/socket.js';
 import express from 'express';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,6 +17,14 @@ server.listen(PORT, () => {
 });
 
 
+
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+app.get("*", (req,res) => {
+  res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
 app.get('/', (req, res) => {
   res.send('Socket.io server running.');
 });
